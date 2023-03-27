2534b7a83698d660671b34531be3b32682c2081e
tyler_7977 <82616410+jake0477@users.noreply.github.com>
2023-03-27 09:28:13 -0500

    Update Server-BusOffGenX.lua

--- Server-BusOffGenX.lua
+++ Server-BusOffGenX.lua
@@ -1,1 +1,28 @@
---Hello
+--Not Done
+
+return function(Vargs)
+	local server, service = Vargs.Server, Vargs.Service
+	server.Commands.ExampleCommand = {
+		Prefix = server.Settings.Prefix;
+		Commands = {"BusOff"};
+		Args = {"Player"};
+		Description = ":BusOff Noob";
+		Hidden = false;
+		Fun = false;
+		AdminLevel = "Admins";-- Admin level
+		Function = function(Player,args)
+			local int = Player.PlayerGui:FindFirstChild("A-Chassis Interface")
+			local car = int.Car.Value
+		if car ~= nil then
+			if car:FindFirstChild("DriveSeat") ~= nil then
+				if car.DriveSeat:IsA("VehicleSeat") then
+					if car.
+            Starter.Value = 0
+            IsOn.Value = false
+            int.Vales.IsOn.Value = false
+					end
+				end
+			end
+		end
+	}
+end
