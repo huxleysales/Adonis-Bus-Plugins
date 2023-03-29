bb646a60fb1bb7f6a2ff7ad7eac28f6f51091a2e
tyler_7977 <82616410+jake0477@users.noreply.github.com>
2023-03-29 10:45:54 -0500

    Update Sever-Commands.lua

--- Route Management Syetem/Sever-Commands.lua
+++ Route Management Syetem/Sever-Commands.lua
@@ -1,1 +1,16 @@
 --Not Ready
+return function(Vargs)
+	local server, service = Vargs.Server, Vargs.Service
+	server.Commands.ExampleCommand = {
+		Prefix = server.Settings.Prefix;
+		Commands = {"Route"};
+		Args = {"School";"Time"};
+		Description = ":Route HS AM";
+		Hidden = false;
+		Fun = false;
+		AdminLevel = "Admins";-- Admin level
+		Function = function(args,args2)
+		  print("Hello World")
+		end
+	}
+end
